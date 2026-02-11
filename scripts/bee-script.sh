b67fc2d9e3a3036e2bcaf64c8f428c711eae3ced
katiepambakianbris <yj23812@bristol.ac.uk>
2026-02-11 08:49:32 +0000

    update script

--- scripts/bee-script.sh
+++ scripts/bee-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=initial_bee_100_trials
+#SBATCH --job-name=bee_hard_mode_only
 #SBATCH --output=results/slurm/%x/%A/%x-%A-run-%a.out    # Standard output
 #SBATCH --error=results/slurm//%x/%A/%x-%A-run-%a.err     # Error log
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
-#SBATCH --time=02:30:00
+#SBATCH --time=01:00:00
 #SBATCH --mem=1000M
 #SBATCH --account=COSC029884
 #SBATCH --array=0-19
@@ -16,6 +16,12 @@
 module add languages/python/3.12.3
  
 SRC_DIR="src_evol"
+
+# make the files 
+
+cd $SRC_DIR
+make 
+cd ..
 
 # running the runscript.py 
 python $SRC_DIR/runscript.py 0 1 $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_JOB_ID $SRC_DIR/bin/main 
